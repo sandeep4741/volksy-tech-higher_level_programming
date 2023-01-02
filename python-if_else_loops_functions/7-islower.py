63d9d02502fba45ad5787c2de1e0e8c413564d2e
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-02 04:16:11 -0800

    7

---
+++ python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def islower(n):
+    if ord(n) >= ord('a') and ord(n) <= ord('z'):
+        return True
+    return False
--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_last_digit(number):
+    abc = ""
     lst = number % 10
     if number < 0:
         lst = lst - 10
