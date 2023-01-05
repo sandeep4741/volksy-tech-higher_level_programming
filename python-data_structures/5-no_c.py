e74827fecfd6cf07bb09644f497102c910901e18
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-05 03:25:22 -0800

    5

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,7 +2,7 @@
 def no_c(my_string):
     res = []
     for i in my_string:
-        if i == "c" or i =="C":
+        if i == "c" or i == "C":
             i = ''
         res.append(i)
     return ("".join(res))
