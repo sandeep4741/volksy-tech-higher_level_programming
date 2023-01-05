c110d8108a9fabaac93685fd9c188a980d6bca0a
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-05 03:20:16 -0800

    5

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    res = ''
+    for i in my_string:
+        if i == "c" or i =="C":
+            i = ''
+        res.append(i)
+    return ("".join(copy))
