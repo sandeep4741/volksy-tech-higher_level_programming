99b370f08a61db4b251911e952f275ade66ba171
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-05 22:03:44 -0800

    10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -4,4 +4,4 @@ def divisible_by_2(my_list=[]):
         if i % 2 == 0:
             print("{:d} {:s} divisible by 2".format(my_list[i], "is")
         else:
-            print("{:d} {:s} divisible by 2".format(my_list[i], "is")
+            print("{:d} {:s} not divisible by 2".format(my_list[i], "is")
