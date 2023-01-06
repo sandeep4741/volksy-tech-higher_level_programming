a342e68022d982a5599804ff96bab04882cf6320
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-05 22:01:17 -0800

    10

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    for i in my_list:
+        if i % 2 == 0:
+            print("{:d} {:s} divisible by 2".format(my_list[i], "is")
+        else:
+            print("{:d} {:s} divisible by 2".format(my_list[i], "is")
