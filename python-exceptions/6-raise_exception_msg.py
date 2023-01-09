1d65ac169fd514e4d9a87d98f5d9b3197334bb17
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-08 23:10:36 -0800

    6

--- python-exceptions/6-raise_exception_msg.py
+++ python-exceptions/6-raise_exception_msg.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception_msg(message=""):
-    try:
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
+    raise NameError(message)
