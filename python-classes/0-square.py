b9e46ac25afcfb732dd9ce0ec39cc4db072d1962
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-09 21:46:18 -0800

    0

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
+'''python3 -c 'print(__import__("my_module").__doc__)'''
+'''python3 -c 'print(__import__("my_module").MyClass.__doc__)'''
 #!/usr/bin/python3
 class Square:
-    #python3 -c 'print(__import__("my_module").my_function.__doc__)
-    #python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)
+    '''python3 -c 'print(__import__("my_module").my_function.__doc__)'''
+    '''python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'''
     pass
 
