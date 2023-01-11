f770d038e42cc5dda05696788060797167dac3d6
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-11 03:19:08 -0800

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-'''square'''
 class Square:
-       """inisialazztion"""
-
-    def __init__(self, size):
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initialises the data"""
+        self.__size = size
         if type(size) != int:
             raise TypeError("size must be an integer")
-        elif size >= 0:
-            raise ValueError("size must be >= zero")
-        else:
-            self.__size = size
+        if size < 0:
+            raise ValueError("size must be >= 0")
