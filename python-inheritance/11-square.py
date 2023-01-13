1ce88e0382aa98dfcaf3fd906e663bc47c490c11
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-13 01:19:23 -0800

    11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+"""initializate"""
+
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
+    """ Square2"""
+
     def __init__(self, size):
-        self.integer_validator(size, size)
+        """initial attributes"""
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
+        self.area()
 
     def __str__(self):
-        return '[Square] ' + str(self.__size) + '/' + str(self.__size)
+        """str"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
