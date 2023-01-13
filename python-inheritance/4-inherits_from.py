4ca397f38f130bf587afb0376562aad2d16dd3de
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-13 00:20:27 -0800

    4

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''class'''
+
+
+def inherits_from(obj, a_class):
+    """Returns True if obj is an instance of a class that inherited"""
+    return isinstance(obj, a_class) and type(obj) is not a_class
