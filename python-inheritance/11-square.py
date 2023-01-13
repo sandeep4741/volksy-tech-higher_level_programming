38ca62b29aa48c331571262fb7f2f43f7061c26d
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-13 00:44:12 -0800

    11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""Geometry module"""
 
+"""
+Square Module
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A class inheriting from the Rectangle class which inherits"""
+    """Square Class"""
+
     def __init__(self, size):
-        """Class constructor with private attributes"""
+        """Initializion of the  class."""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def area(self):
-        """Method to calculate area """
+        """Returns the area of the square"""
         return self.__size ** 2
 
     def __str__(self):
-        """return string"""
-        return str("[Square] {}/{}".format(self.__size, self.__size))
+        """Returns a string representation of the class"""
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
