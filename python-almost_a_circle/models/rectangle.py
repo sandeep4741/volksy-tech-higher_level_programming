9b045582e12415f8ed85bc7dc9de608e21bd934b
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-22 23:44:33 -0800

    2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,49 +1,100 @@
- def setUp(cls):
-        """ Resets object to 0. For id """
-        Base._Base__nb_objects = 0
-
-    def test_rec(self):
-        """ Test for valid rectangle """
-        rec = Rectangle(1, 5)
-        self.assertEqual(type(rec.width), int)
-        self.assertEqual(type(rec.height), int)
-        self.assertEqual((rec.width, rec.height, rec.x, rec.y, rec.id),
-                         (1, 5, 0, 0, 1))
-        rec = Rectangle(3, 3, 1)
-        self.assertEqual((rec.width, rec.height, rec.x, rec.y, rec.id),
-                        (3, 3, 1, 0, 2))
-        with self.assertRaises(TypeError):
-            rec = Rectangle("Potato", 1)
-        with self.assertRaises(ValueError):
-            rec = Rectangle(5, -99)
-
-    def test_area(self):
-        """ Test for correct area calculation """
-        rec = Rectangle(5, 5)
-        self.assertEqual(rec.area(), 25)
-        rec = Rectangle(6, 11, 2, 7, 32767)
-        self.assertEqual(rec.area(), 66)
-
-    def test_text(self):
-        """Tests for correct syntax """
-        rec = Rectangle(9, 7, 8, 2, 5)
-        rec_line = "[Rectangle] ({}) {}/{} - {}/{}".format(
-                rec.id, rec.x, rec.y, rec.width, rec.height)
-        self.assertEqual(str(rec), rec_line)
-        rec.update(77)
-        rec_line = "[Rectangle] ({}) {}/{} - {}/{}".format(
-                rec.id, rec.x, rec.y, rec.width, rec.height)
-        self.assertEqual(str(rec), rec_line)
-        rec.update(55, 6, 1)
-        rec_line = "[Rectangle] ({}) {}/{} - {}/{}".format(
-                rec.id, rec.x, rec.y, rec.width, rec.height)
-        self.assertEqual(str(rec), rec_line)
-        rec.update(id=9853, width=10, y=0)
-        rec_line = "[Rectangle] ({}) {}/{} - {}/{}".format(
-                rec.id, rec.x, rec.y, rec.width, rec.height)
-        self.assertEqual(str(rec), rec_line)
+#!/usr/bin/python3
+# rectangle.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a rectangle class."""
+from models.base import Base
 
 
-if __name__ == '__main__':
-    unittest.main()
--- INSERT --                               
+class Rectangle(Base):
+    """Represent a rectangle."""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialize a new Rectangle.
+        Args:
+            width (int): The width of the new Rectangle.
+            height (int): The height of the new Rectangle.
+            x (int): The x coordinate of the new Rectangle.
+            y (int): The y coordinate of the new Rectangle.
+            id (int): The identity of the new Rectangle.
+        Raises:
+            TypeError: If either of width or height is not an int.
+            ValueError: If either of width or height <= 0.
+            TypeError: If either of x or y is not an int.
+            ValueError: If either of x or y < 0.
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Set/get the width of the Rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Set/get the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """Set/get the x coordinate of the Rectangle."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """Set/get the y coordinate of the Rectangle."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """Return the area of the Rectangle."""
+        return self.width * self.height
+
+    def display(self):
+        """Print the Rectangle using the `#` character."""
+        if self.width == 0 or self.height == 0:
+            print("")
+            return
+
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def update(self, *args, **kwargs):
+
