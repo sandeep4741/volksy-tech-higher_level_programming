3f360e67d3bd488b70af5dc6744dd87c4a5fe839
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-31 01:54:32 -0800

    1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER 
 IF NOT EXISTS 'user_0d_1'@'localhost'
 IDENTIFIED by 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON '*'
+GRANT ALL PRIVILEGES ON *.*
 TO 'user_0d_1'@'localhost'
 IDENTIFIED by 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
