44dea9f6b658b2807c9738935d044c9151a0ea3d
sandeep4741 <sandeepg@brightcomgroup.com>
2023-01-31 02:54:25 -0800

    6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- create database and
+-- create table if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT unique AUTO_INCREMENT NOT NULL,
+        name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id)
+);
