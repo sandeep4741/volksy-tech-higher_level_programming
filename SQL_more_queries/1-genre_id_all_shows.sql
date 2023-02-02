6832b27fb690a99f9ef104a8feac29be1d0bf159
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-01 22:43:47 -0800

    11

--- SQL_more_queries/1-genre_id_all_shows.sql
+++ SQL_more_queries/1-genre_id_all_shows.sql
@@ -1,6 +1,5 @@
 -- task 11. Genre ID for all shows
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_show_genres.show_id = tv_shows.id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT tv_shows.title, tv_show_genres.genre_id 
+FROM tv_shows 
+LEFT JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id 
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
