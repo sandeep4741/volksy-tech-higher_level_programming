1c29fd1439b038cf713eb3c32a2ba4c093a0adc3
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-06 23:14:07 -0800

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
-"""takes in arguments and displays all values in the states"""
+"""SQL Injection"""
+
+
+import sys
 import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name = %(name)",
-                   {'name': argv[4]})
-    for data in cursor.fetchall():
-        print(data)
-    cursor.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    for i in c:
+        if i[1] == sys.argv[4]:
+            print(i)
