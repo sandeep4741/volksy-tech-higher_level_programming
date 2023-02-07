dd024f7ec29c3721d00351dca6ea2b8b76d6ac31
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-06 22:58:55 -0800

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
 """takes in arguments and displays all values in the states"""
+import sys
 import MySQLdb
-from sys import argv
-
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=argv[1], passwd=argv[2], db=argv[3])
-    cur = db.cursor()
-    sql = "SELECT * FROM states WHERE name=%s ORDER BY states.id"
-    num_rows = cur.execute(sql, (argv[4], ))
-    for i in range(num_rows):
-        print(cur.fetchone())
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
