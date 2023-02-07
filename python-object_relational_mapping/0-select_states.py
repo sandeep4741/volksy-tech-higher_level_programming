70c7959a37cb2dc4be929c440a4eb4d86034dee9
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-06 21:53:49 -0800

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,31 +1,13 @@
 #!/usr/bin/python3
-""" Importing MySQLdb for database tasks. Sys is for retrieving
-    command line arguments """
+"""lists all states from the database"""
 import MySQLdb
-import sys
-""" 0-select_states.py """
+from sys import argv
 
 
-def getStates():
-    """
-    Driving function. Actually not necessary, but done to make
-    the entire code nicer to read.
-    Accesses the database with user and password, all passed in,
-    to localhost at port 3306. Then, retrieve all information
-    from the states database and prints them all out
-    """
-    mydb = MySQLdb.connect(host='localhost',
-                           user=sys.argv[1],
-                           passwd=sys.argv[2],
-                           db=sys.argv[3],
-                           port=3306)
-
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM states ORDER BY states.id")
-    myresult = mycursor.fetchall()
-
-    for x in myresult:
-        print(x)
-
-if __name__ == '__main__':
-    getStates()
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id")
+    for i in range(num_rows):
+        print(cur.fetchone())
