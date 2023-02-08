75587f91b35e0da77b9346e561413af7dc8dfc01
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-08 01:03:46 -0800

    5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    sql = "select cities.name from cities left join states on states.id=cities.state_id where states.id=3;"
-    num_rows = cur.execute(sql)
+    sql = "select cities.name from cities left join states on states.id=cities.state_id where states.id=%s;"
+    num_rows = cur.execute(sql,(arg[4])
     for i in range(num_rows):
         print(cur.fetchone())
     cur.close()
