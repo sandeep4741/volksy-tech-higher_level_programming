1fed984ecd2dada87c687f76bd0226e7ac2428c7
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-07 22:27:01 -0800

    4

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -9,7 +9,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=argv[1], passwd=argv[2], db=argv[3])
     cur = db.cursor()
-    sql = "SELECT cities FROM cities"
+    sql = "SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN states ON cities.state_id = states.id ORDER BY cities.id"
     num_rows = cur.execute(sql)
     for i in range(num_rows):
         print(cur.fetchone())
