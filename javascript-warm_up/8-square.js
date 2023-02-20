46ae7ed2a05e5a4fcb66b7c4debb03c06b819c6a
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-20 04:17:23 -0800

    8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
 
-let a = parseInt(process.argv[2])
+let square = '';
 if (process.argv.length === 2 || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < a; i++) {
-    console.log('x'.repeat(a));
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    square = '';
+    for (let j = 0; j < parseInt(process.argv[2]); j++) { square += 'X'; }
+    console.log(square);
+  }
 }
