e72ddf0e091a35b185ab8cb8660a4feaeba2003b
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-20 02:36:06 -0800

    6

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,3 +1,6 @@
 #!/usr/bin/node
 
-console.log('C is fun\nPython is cool\nJavaScript is amazing');
+const lang = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (const l in lang) {
+  console.log(lang[l]);
+}
