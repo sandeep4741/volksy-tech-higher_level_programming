0847814b4ad0540983ba6250570ffdce3f292663
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-20 01:53:32 -0800

    11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-if (process.argv < 3){
+if (process.argv <= 3){
   console.log(0);
 } else {
-  let a = process.argv;
+  const a = process.argv;
   a.sort()
   console.log(a[a.length - 2]);
 }
