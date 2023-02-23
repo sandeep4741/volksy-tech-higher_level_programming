ae4ee1f99b1644ab928d736ca7c9434c364ed558
sandeep4741 <sandeepg@brightcomgroup.com>
2023-02-22 20:26:01 -0800

    3

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -4,8 +4,8 @@ module.exports = class Rectangle {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-  }
-}	  
+    }
+  }	  
 
   print () {
     for (let i = 0; i < this.height; i++)
